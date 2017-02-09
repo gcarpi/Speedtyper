56051a4c3a69b20bb9a3d47c6d8b73dd836811ff
gcarpi <gcarpi@hotmail.com>
2017-02-09 14:42:35 -0200

    Speedtyper first commit

---
+++ public/js/slider.js
@@ -0,0 +1,12 @@
+$(function() {
+	$('.slider').slick({
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		adaptiveHeight: true,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3000
+	});
+});
